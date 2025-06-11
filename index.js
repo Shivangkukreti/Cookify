d19e112bbf25cd41464fd7f777d99300de908ac0
Shivangkukreti <shivangkukretiwork@gmail.com>
2025-06-12 00:52:50 +0530

    2

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const {storage} = require('./cloud');
 const upload = multer({ storage });
 
 
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.log("MongoDB connection error:", err));
+
+
 const recipes=require("./models/recipe")
 const user=require("./models/user");
 const reviews=require("./models/review");
